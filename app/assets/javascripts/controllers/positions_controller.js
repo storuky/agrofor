4d6bb024f000fd36650e99ed1c581f3aa8d72e1b
Павел Кононенко <storuky@gmail.com>
2015-09-21 00:21:56 +0300

    fix

--- app/assets/javascripts/controllers/positions_controller.js
+++ app/assets/javascripts/controllers/positions_controller.js
@@ -1,9 +1,9 @@
 app.controller('PositionsController', ['$scope', 'Page', 'method', '$position', function ($scope, Page, method, $position) {
   var ctrl = this;
 
-  Page.current = 'positions';
 
   if (method=='new') {
+    Page.current = 'new_positions';
     ctrl.position = {
       lat: 55.7,
       lng: 37.6
@@ -11,6 +11,7 @@ app.controller('PositionsController', ['$scope', 'Page', 'method', '$position', 
   }
 
   if (method=='index') {
+    Page.current = 'positions';
     ctrl.positions = $position.query({status: 'opened'});
     ctrl.tab = 'opened';
   }
