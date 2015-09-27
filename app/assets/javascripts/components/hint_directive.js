fa0ea689f4ddc8fffdc64921d524b03d4efd8303
Павел Кононенко <storuky@gmail.com>
2015-09-27 23:02:45 +0300

    fix

--- app/assets/javascripts/components/hint_directive.js
+++ app/assets/javascripts/components/hint_directive.js
@@ -4,7 +4,7 @@ app.directive('hint', ['$compile', function ($compile) {
     // name: '',
     // priority: 1,
     // terminal: true,
-    scope: true, // {} = isolate, true = child, false/undefined = no change
+    // scope: true, // {} = isolate, true = child, false/undefined = no change
     // controller: function($scope, $element, $attrs, $transclude) {},
     // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
     restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
@@ -14,9 +14,7 @@ app.directive('hint', ['$compile', function ($compile) {
     // transclude: true,
     // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
     link: function($scope, iElm, iAttrs, controller) {
-      $scope.hint = iAttrs.hint;
-      $scope.place = iAttrs.place;
-      var hint = $compile('<div ng-show="Page.showHint" class="hint {{place}}"><div class="hint__point"></div><div class="hint__line"></div><div class="hint__text">{{hint}}</div></div>')($scope);
+      var hint = $compile('<div ng-show="Page.showHint" class="hint '+iAttrs.place+'"><div class="hint__point"></div><div class="hint__line"></div><div class="hint__text">'+iAttrs.hint+'</div></div>')($scope);
 
       iElm.after(hint)
 
